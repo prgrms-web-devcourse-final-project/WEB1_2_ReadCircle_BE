7a66e3e24a60d27c8db118a997dd77efa56c0278
Geontae-Park <tlals0028@gmail.com>
2024-11-18 12:25:51 +0900

    Initial commit
    Initial commit

---
+++ src/main/java/org/prgrms/devcourse/readcircle/ReadCircleApplication.java
@@ -0,0 +1,13 @@
+package org.prgrms.devcourse.readcircle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadCircleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReadCircleApplication.class, args);
+    }
+
+}
