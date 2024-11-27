50b1b1a468343168df852a786afa95639c9a3353
EGHyeon <kahyun1890@gmail.com>
2024-11-27 10:24:09 +0900

    Fix 매입 상태 수정(PurchaseStatus)

--- src/main/java/org/prgrms/devcourse/readcircle/domain/purchase/entity/enums/PurchaseStatus.java
+++ src/main/java/org/prgrms/devcourse/readcircle/domain/purchase/entity/enums/PurchaseStatus.java
@@ -1,7 +1,6 @@
 package org.prgrms.devcourse.readcircle.domain.purchase.entity.enums;
 
 public enum PurchaseStatus {
-    REQUEST_WAITING,   //신청 완료
-    PRICE_SET,          //가격 책정
-    DEPOSIT_COMPLETE    //정산 완료
+    RESPONSE_WAITING,   //신청 완료
+    PRICE_SET         //가격 책정
 }
