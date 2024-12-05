a07c2b7b789e01826d730a0fca4630def154d341
EGHyeon <kahyun1890@gmail.com>
2024-12-05 14:25:27 +0900

    Fix: SSE 수정

---
+++ src/main/java/org/prgrms/devcourse/readcircle/common/notification/dto/MessageDTO.java
@@ -0,0 +1,10 @@
+package org.prgrms.devcourse.readcircle.common.notification.dto;
+
+public class MessageDTO {
+
+    private String message;
+
+    public MessageDTO(String message){ this.message = message;}
+    public String getMessage(){ return message; }
+    public void setMessage(String message){ this.message = message; }
+}
--- src/main/java/org/prgrms/devcourse/readcircle/common/notification/entity/Notification.java
+++ src/main/java/org/prgrms/devcourse/readcircle/common/notification/entity/Notification.java
@@ -2,6 +2,7 @@ package org.prgrms.devcourse.readcircle.common.notification.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.prgrms.devcourse.readcircle.common.BaseTimeEntity;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
@@ -15,19 +16,15 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @NoArgsConstructor
 @EntityListeners(AuditingEntityListener.class)
-public class Notification {
+public class Notification extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long notificationId;
 
-    private String userId;
+    private String toUserId;
 
     @Enumerated(EnumType.STRING)
     private NotificationType type;
 
     private String message;
-
-    @CreatedDate
-    private LocalDateTime createdAt;
-
 }
