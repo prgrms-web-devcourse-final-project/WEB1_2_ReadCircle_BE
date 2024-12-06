4ed996e692170bdc27654419349b15a634c6e46c
EGHyeon <kahyun1890@gmail.com>
2024-12-06 15:44:04 +0900

    Fix: SSE 스케줄러 설정

--- src/main/java/org/prgrms/devcourse/readcircle/common/notification/repository/NotificationRepository.java
+++ src/main/java/org/prgrms/devcourse/readcircle/common/notification/repository/NotificationRepository.java
@@ -2,11 +2,16 @@ package org.prgrms.devcourse.readcircle.common.notification.repository;
 
 import org.prgrms.devcourse.readcircle.common.notification.entity.Notification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
-    @Query(" SELECT n FROM Notification n WHERE n.toUserId = :toUserId ")
+    @Query("SELECT n FROM Notification n WHERE n.toUserId = :toUserId")
     List<Notification> findByToUserId(String toUserId);
+
+    @Modifying
+    @Query(value = "DELETE FROM Notification WHERE created_at < now() - interval 1 DAY; ", nativeQuery = true)
+    void deleteNotifications();
 }
